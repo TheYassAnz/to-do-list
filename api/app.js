272fc7a42fb9608484b9c6faa43407672089ffd8
TheYassAnz <yassanz.contact@gmail.com>
2024-10-28 15:06:10 +0100

    feat: install and init prettier

--- api/app.js
+++ api/app.js
@@ -1,21 +1,21 @@
 // import express
-const express = require('express');
+const express = require("express");
 
 // import mongoose
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 // import dotenv
-require('dotenv').config();
-const p = process.env
+require("dotenv").config();
+const p = process.env;
 
 // connect to the database
-mongoose.connect(`${p.MONGODB_URL}`,
-    {
+mongoose
+    .connect(`${p.MONGODB_URL}`, {
         useNewUrlParser: true,
-        useUnifiedTopology: true
+        useUnifiedTopology: true,
     })
-    .then(() => console.log('Connect to MongoDB successful !'))
-    .catch((error) => console.log('Connect to MongoDB failed !'));
+    .then(() => console.log("Connect to MongoDB successful !"))
+    .catch((error) => console.log("Connect to MongoDB failed !"));
 
 // create an express app
 const app = express();
@@ -23,24 +23,30 @@ const app = express();
 // Middleware which intercept JSON data
 app.use(express.json());
 
-const taskRoutes = require('./routes/task')
-const userRoutes = require('./routes/user');
+const taskRoutes = require("./routes/task");
+const userRoutes = require("./routes/user");
 
 // cors middleware
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization",
+    );
+    res.setHeader(
+        "Access-Control-Allow-Methods",
+        "GET, POST, PUT, DELETE, PATCH, OPTIONS",
+    );
     next();
 });
 
 // simple middleware
-app.get('/api', (req, res) => {
-    res.json({ message: 'Hello World!' });
-})
+app.get("/api", (req, res) => {
+    res.json({ message: "Hello World!" });
+});
 
-app.use('/api/tasks', taskRoutes);
-app.use('/api/auth', userRoutes);
+app.use("/api/tasks", taskRoutes);
+app.use("/api/auth", userRoutes);
 
 // export the express app
 module.exports = app;
--- api/middleware/auth.js
+++ api/middleware/auth.js
@@ -1,17 +1,17 @@
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 
 module.exports = (req, res, next) => {
     try {
         // Get the token from the headers
-        const token = req.headers.authorization.split(' ')[1];
+        const token = req.headers.authorization.split(" ")[1];
         // Decode the token
-        const decodedToken = jwt.verify(token, 'RANDOM_TOKEN_SECRET');
+        const decodedToken = jwt.verify(token, "RANDOM_TOKEN_SECRET");
         // Get the user ID from the token
         req.auth = {
-            userId: decodedToken.userId
+            userId: decodedToken.userId,
         };
         next();
     } catch (error) {
         res.status(401).json({ error });
     }
-}
+};
