62660474ffe7a2cc848aa8df898246e6cb767132
TheYassAnz <yassanz.contact@gmail.com>
2024-10-28 16:35:59 +0100

    feat: show tasks

--- frontend/src/pages/Home/index.jsx
+++ frontend/src/pages/Home/index.jsx
@@ -1,12 +1,38 @@
 import useAuthUser from "react-auth-kit/hooks/useAuthUser";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [tasks, setTasks] = useState([]);
   const auth = useAuthUser();
+
+  const fetchTasks = () => {
+    axios
+      .get(`${process.env.REACT_APP_API_URI}/tasks`)
+      .then((response) => {
+        console.info("response:", response.data);
+        setTasks(response.data);
+      })
+      .catch((error) => console.error("error:", error));
+  };
+
+  useEffect(() => {
+    fetchTasks();
+  }, []);
+
   return (
-    <div className="flex flex-col items-center justify-center h-screen">
-      <h1 className="text-4xl">
+    <div className="">
+      <h3 className="text-2xl">
         Bienvenue <strong>{auth.firstname + " " + auth.lastname} </strong>
-      </h1>
+      </h3>
+      <div>
+        <h4 className="text-xl">Vos tâches:</h4>
+        <ul className="list-disc">
+          {tasks.map((task) => (
+            <li key={task._id}>{task.name}</li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
